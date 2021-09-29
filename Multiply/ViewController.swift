208fe9ad9f9abd4e78f7141ee8bcca3ff849d1eb
Jackson <jhhemme43@gmail.com>
2021-09-29 11:02:18 -0600

    MVP

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -5,15 +5,42 @@
 //  Created by Jackson Hemme on 9/28/21.
 //
 
+
+
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+// variables
+    
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    
+    @IBOutlet weak var secondNumber: UITextField!
+    
+    
+    @IBOutlet weak var productLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answerNumber(_ sender: Any)
+    {
+        var Num1 = firstNumber.text ?? "0"
+        var intergerNumber1 = Float (Num1) ?? 0.0
+        var Num2 = secondNumber.text ?? "0"
+        var intergerNumber2 = Float (Num2) ?? 0.0
+        
+        var product = intergerNumber1 * intergerNumber2
+        
+        productLabel.text = "\(product)"
+        
+        
+    }
+   
+    
 }
 
